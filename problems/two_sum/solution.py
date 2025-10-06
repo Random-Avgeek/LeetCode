8631a21a068659549c16b2d92e8442cbbc645d12
Arush <160302555+Random-Avgeek@users.noreply.github.com>
2025-10-06 05:46:55 +0000

    Sync LeetCode submission - Two Sum (python3)

---
+++ problems/two_sum/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nummap={}
+        for i in range(0,len(nums)):
+            complement = target - nums[i]
+            if complement in nummap:
+                return [nummap[complement],i]
+            nummap[nums[i]]=i
+                
