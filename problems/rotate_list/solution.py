006f8d2e02b9a2fb29ab627365ad3aee07178ccf
Arush <160302555+Random-Avgeek@users.noreply.github.com>
2026-06-14 04:50:41 +0000

    Sync LeetCode submission - Rotate List (python3)

---
+++ problems/rotate_list/solution.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head or not head.next or k==0:
+            return head
+        curr = head
+        count=1
+        while curr.next!=None:
+            curr=curr.next
+            count+=1
+        curr.next=head
+        k=k%count
+
+        newtailsteps=count-k-1
+        newtail=head
+        for _ in range(newtailsteps):
+            newtail=newtail.next
+        newhead=newtail.next
+        newtail.next=None
+
+        return newhead
