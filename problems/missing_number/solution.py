a3b4bd9609e50ba092c741a53c656cf483c5f1ac
Arush <160302555+Random-Avgeek@users.noreply.github.com>
2026-07-06 11:46:31 +0000

    Sync LeetCode submission - Missing Number (python3)

---
+++ problems/missing_number/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n=len(nums)
+        expectedsum=0
+        for i in range(1,n+1):
+            expectedsum+=i
+        actualsum=sum(nums)
+        return (expectedsum-actualsum)
