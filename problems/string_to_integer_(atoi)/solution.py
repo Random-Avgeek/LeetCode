7c1ce6e773e743fdd7ce243a7783d4a6c0a90b33
Arush <160302555+Random-Avgeek@users.noreply.github.com>
2026-07-13 04:56:56 +0000

    Sync LeetCode submission - String to Integer (atoi) (python3)

--- problems/string_to_integer_(atoi)/solution.py
+++ problems/string_to_integer_(atoi)/solution.py
@@ -1,28 +1,22 @@
+INT_MAX=2**31-1
+INT_MIN=-2**31
+
 class Solution:
+    def helper(self,s:str,i:int,num:int,sign):
+        if i>=len(s) or not s[i].isdigit():
+            return sign*num
+        num=num*10+int(s[i])
+        if sign*num<=INT_MIN: return INT_MIN
+        elif sign*num>=INT_MAX: return INT_MAX
+        return self.helper(s,i+1,num,sign)
     def myAtoi(self, s: str) -> int:
-        n = len(s)
-        i = 0
+        i=0
+        while i < len(s) and s[i]==' ':
+            i+=1
+        sign=1
+        if i<len(s) and (s[i]=='-' or s[i]=='+'):
+            sign=-1 if s[i]=='-' else 1
+            i+=1
+        return self.helper(s,i,0,sign)
+
         
-        while i < n and s[i] == ' ':
-            i += 1
-            
-        isNegative = False
-        if i < n and (s[i] == '+' or s[i] == '-'):
-            if s[i] == '-':
-                isNegative = True
-            i += 1
-        final_num = 0
-        while i < n and s[i].isdigit():
-            final_num = final_num * 10 + int(s[i])
-            i += 1
-        if isNegative:
-            final_num = -final_num
-        INT_MIN = -2**31
-        INT_MAX = 2**31 - 1
-        
-        if final_num < INT_MIN:
-            return INT_MIN
-        if final_num > INT_MAX:
-            return INT_MAX
-            
-        return final_num
