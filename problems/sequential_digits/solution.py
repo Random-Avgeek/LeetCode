2b7edac0157b275dab3a766c19d95681f1f083ef
Arush <160302555+Random-Avgeek@users.noreply.github.com>
2026-07-13 03:07:22 +0000

    Sync LeetCode submission - Sequential Digits (python3)

---
+++ problems/sequential_digits/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        arr=[]
+        digits="123456789"
+
+        for length in range(len(str(low)),len(str(high))+1):
+            for start in range(10-length):
+                num = int(digits[start:start+length])
+                if low <= num <= high:
+                    arr.append(num)
+
+        return arr
