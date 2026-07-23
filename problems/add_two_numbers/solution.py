7ff1dacd9310bfb994fa5e80a56dcdb08a80a4b0
Arush <160302555+Random-Avgeek@users.noreply.github.com>
2026-07-23 05:13:16 +0000

    Sync LeetCode submission - Add Two Numbers (python3)

--- problems/add_two_numbers/solution.py
+++ problems/add_two_numbers/solution.py
@@ -5,20 +5,21 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        temp=ListNode()
-        resnode=temp
-        carry=0
-        total=0
+        temp = ListNode()
+        resnode = temp
+        total = 0
+        carry = 0
         while l1 or l2 or carry:
             total = carry
             if l1:
                 total+=l1.val
                 l1=l1.next
             if l2:
-                total += l2.val
+                total+=l2.val
                 l2=l2.next
             num = total%10
-            carry = total // 10
-            temp.next=ListNode(num)
+            carry = total//10
+            n1  = ListNode(num,None)
+            temp.next=n1
             temp=temp.next
         return resnode.next
